c2055b97e78ac1260a4a85c7d093caf0f0b3f564
canberkb8 <canberkbibican@hotmail.com>
2021-10-16 14:13:51 +0300

    initialize commit

---
+++ MovieSwift/Core/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSwift
+//
+//  Created by Canberk Bibican on 16.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
